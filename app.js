fcc2d28ca7a25c17e6f3212f11dc297ae2d0ccce
amldev <andreliongo85@outlook.com>
2023-12-04 12:32:23 +0100

    premier-commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// form 
+document.querySelector('form').style["background"] = "#414";
+document.querySelector('form').style["display"] = "grid";
+document.querySelector('form').style["padding"] = "60vh";
+document.querySelector('input').style["color"] = "#000";
+document.querySelector('textarea').style["color"] = "black";
+// bdd
